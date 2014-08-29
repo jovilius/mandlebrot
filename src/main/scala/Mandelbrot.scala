fb74d866eda533cb8eb1585059c02876741315e2
Giulio De Luise <giuliodeluise@timeout.com>
2014-08-29 21:53:01 +0100

    Improve `Orbit`.

--- src/main/scala/Mandelbrot.scala
+++ src/main/scala/Mandelbrot.scala
@@ -6,18 +6,22 @@ import Math._
 import Complex._
 import Orbit._
 
-case class Orbit(c: Complex, x: Complex = 0, n: Int = 20) {
-  def points(z: Complex = quadraticPolynomial(c, x)): Stream[Complex] = z #:: points(quadraticPolynomial(c, z))
-  def isBounded(radius: Double = 2): Boolean = points().take(n).forall(!_ < radius)
+case class Orbit(c: Complex, x: Complex = 0, maxPoints: Int = MaxPoints, maxRadius: Double = 3) {
+  private def magnitude(point: Complex): Double = !(point - c)
+  private def points(z: Complex = quadraticPolynomial(c, x)): Stream[Complex] = z #:: points(quadraticPolynomial(c, z))
+  def points: Stream[Complex] = points().take(maxPoints).takeWhile(magnitude(_) < maxRadius)
+  def lenght: Int = points.size
+  def isBounded: Boolean = lenght == maxPoints
 }
 
 object Orbit {
+  val MaxPoints = 100
   def quadraticPolynomial(c: Complex, x: Complex) = x * x + c
 }
 
 object Mandelbrot {
   def main(args: Array[String]) {
-    val set = plane(z(-2, -1), z(0.5, 1), 80).map(_.map(c => Orbit(c).isBounded()))
+    val set = plane(z(-2, -1.1), z(0.5, 1.1), 80).map(_.map(c => Orbit(c).isBounded))
     val ascii = set.map(_.map(if (_) '*' else ' ').mkString).mkString("\n")
     println(ascii)    
   }
